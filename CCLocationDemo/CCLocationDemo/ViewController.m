47ab06987bfb08f985789a9d6694a8414088fc0b
RachalZhou <1048255049@qq.com>
2018-08-28 15:55:37 +0800

    add demo icon
    add demo icon

--- CCLocationDemo/CCLocationDemo/ViewController.m
+++ CCLocationDemo/CCLocationDemo/ViewController.m
@@ -9,12 +9,15 @@
 #import "ViewController.h"
 #import "CCLocationFetcher.h"
 
+#define kScreenW CGRectGetWidth([UIScreen mainScreen].bounds)
+#define kScreenH CGRectGetHeight([UIScreen mainScreen].bounds)
+
 @interface ViewController ()
 
 @property (nonatomic, readwrite, strong) UILabel *label1;
 @property (nonatomic, readwrite, strong) UILabel *label2;
 
-@property (nonatomic, readwrite, strong) CCLocationFetcher *fetcher;
+@property (nonatomic, readwrite, strong) CCLocationFetcher *fetcher2;
 
 @end
 
@@ -26,22 +29,31 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     //测试Label和Button
-    
-    _label1 = [[UILabel alloc] initWithFrame:CGRectMake(15, 90, 350, 30)];
+    _label1 = [[UILabel alloc] init];
+    _label1.bounds = CGRectMake(0, 0, kScreenW - 30, 30);
+    _label1.center = CGPointMake(kScreenW / 2, 100);
+    _label1.textAlignment = NSTextAlignmentCenter;
     _label1.backgroundColor = [UIColor greenColor];
     [self.view addSubview:_label1];
     
-    UIButton *btn1 = [[UIButton alloc] initWithFrame:CGRectMake(15, 140, 150, 30)];
+    UIButton *btn1 = [[UIButton alloc] init];
+    btn1.bounds = CGRectMake(0, 0, 150, 30);
+    btn1.center = CGPointMake(kScreenW / 2, 140);
     btn1.backgroundColor = [UIColor redColor];
     [btn1 setTitle:@"定位按钮1" forState:UIControlStateNormal];
     [btn1 addTarget:self action:@selector(click1) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn1];
     
-    _label2 = [[UILabel alloc] initWithFrame:CGRectMake(15, 190, 350, 30)];
+    _label2 = [[UILabel alloc] init];
+    _label2.bounds = CGRectMake(0, 0, kScreenW - 30, 30);
+    _label2.center = CGPointMake(kScreenW / 2, 180);
+    _label2.textAlignment = NSTextAlignmentCenter;
     _label2.backgroundColor = [UIColor yellowColor];
     [self.view addSubview:_label2];
     
-    UIButton *btn2 = [[UIButton alloc] initWithFrame:CGRectMake(15, 240, 150, 30)];
+    UIButton *btn2 = [[UIButton alloc] init];
+    btn2.bounds = CGRectMake(0, 0, 150, 30);
+    btn2.center = CGPointMake(kScreenW / 2, 220);
     btn2.backgroundColor = [UIColor redColor];
     [btn2 setTitle:@"定位按钮2" forState:UIControlStateNormal];
     [btn2 addTarget:self action:@selector(click2) forControlEvents:UIControlEventTouchUpInside];
@@ -65,8 +77,8 @@
     __weak typeof(self) weakSelf = self;
     
     //使用方法二
-    _fetcher = [[CCLocationFetcher alloc] init];
-    [_fetcher startWithCompletionHandler:^(LocationModel *location) {
+    _fetcher2 = [[CCLocationFetcher alloc] init];
+    [_fetcher2 startWithCompletionHandler:^(LocationModel *location) {
         
         NSLog(@"经度:%@--纬度:%@--地址:%@",location.longitude,location.latitude,location.address);
         weakSelf.label2.text = [NSString stringWithFormat:@"经度:%@ 纬度:%@",location.longitude,location.latitude];
---
+++ CCLocationFetcher.podspec
@@ -0,0 +1,15 @@
+Pod::Spec.new do |s|
+
+s.name = "CCLocationFetcher"
+s.version = "0.0.1"
+s.summary = "a location fetcher"
+s.description = "a class that can get location info easily"
+s.homepage = "https://github.com/RachalZhou/CCLocationFetcher"
+s.license = { :type => 'MIT', :file => 'LICENSE' } 
+s.author = { "RachalZhou" => "zrcrachal@qq.com" } 
+s.platform = :ios, "8.0"
+s.source = { :git => "https://github.com/RachalZhou/CCLocationFetcher.git", :tag => "v#{s.version}" }
+s.source_files = 'CCLocationFetcher/CCLocationDemo/Code/*'
+s.requires_arc = true
+
+end
